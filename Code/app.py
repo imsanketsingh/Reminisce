787f1266bed5d353d898a00546aea59b3159e070
imsanketsingh <imsanketsingh@gmail.com>
2023-04-22 12:29:22 +0530

    Updated

--- Code/app.py
+++ Code/app.py
@@ -93,8 +93,8 @@ elif choose == "Articles":
 
         def show_pdf(file_path):
             with open(file_path,"rb") as f:
-                base64_pdf = binascii.hexlify(f.read()).decode('utf-8')
-            pdf_display = f'<iframe  src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+            pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></embed>'
             st.markdown(pdf_display, unsafe_allow_html=True)
         st.write('\n')
 
