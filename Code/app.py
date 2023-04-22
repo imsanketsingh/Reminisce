5d978ec703bb74bf681ceceb5522ae23dd32b6b8
imsanketsingh <imsanketsingh@gmail.com>
2023-04-22 14:39:20 +0530

    Updated

--- Code/app.py
+++ Code/app.py
@@ -92,6 +92,7 @@ elif choose == "Articles":
 
         def show_pdf(file_path):
             with open(file_path,"rb") as f:
+                st.markdown("YESSSSSS")
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
             pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf"></embed>'
             st.markdown(pdf_display, unsafe_allow_html=True)
