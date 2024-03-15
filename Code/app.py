4c0cfc57f1313dd8eb0488d436aaed583205474b
imsanketsingh <imsanketsingh@gmail.com>
2024-03-15 16:35:27 +0530

    Changes

--- Code/app.py
+++ Code/app.py
@@ -209,7 +209,7 @@ if choose == "Compendia":
                 font-size:22px ; font-family: 'Black'; color: #FFFFF;}
                 </style> """, unsafe_allow_html=True)
                 st.markdown('<p class="font">Rashmirathi : Part 1</p>', unsafe_allow_html=True)    
-                st.markdown('This is the first article of the series "Rashmirathi" that explores this great epic by Ramdhari Singh Dinkar, delving into its philosophical and literary themes, offering insights into duty, morality, and the complexities of the Mahabharata character, Karna.', unsafe_allow_html=True)
+                st.markdown('The first article of the series "Rashmirathi" that explores this great epic by Ramdhari Singh Dinkar, delving into its philosophical and literary themes, offering insights into duty, morality, and the complexities of the Mahabharata character, Karna.', unsafe_allow_html=True)
             if st.button("Get into it", key="mybutton"):
                 showthecontent('./New/Books/Rashmirathi.html')
                 st.button("Wrap it up!", help="Close it")
@@ -242,7 +242,7 @@ if choose == "Compendia":
                 font-size:22px ; font-family: 'Black'; color: #FFFFF;}
                 </style> """, unsafe_allow_html=True)
                 st.markdown('<p class="font">The Role of Statistics in Data Science</p>', unsafe_allow_html=True)    
-                st.markdown('Discover the crucial role of statistics in Data Science. Explore how statistical techniques empower data exploration, cleaning, hypothesis testing, predictive modeling, feature selection, and model evaluation', unsafe_allow_html=True)
+                st.markdown('Discover the crucial role of statistics in Data Science. Explore how statistical techniques empower data exploration, cleaning, hypothesis testing, predictive modeling, feature selection, and model evaluation.', unsafe_allow_html=True)
             if st.button("Get into it", key="mybutton"):
                 showthecontent('./New/Tech/RoleofStats.html')
                 st.button("Wrap it up!", help="Close it")
