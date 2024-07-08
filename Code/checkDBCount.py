e725067c6eb961d54fe4af329a5448914a010dda
imsanketsingh <imsanketsingh@gmail.com>
2024-07-08 14:13:18 +0530

    changes

--- Code/checkDBCount.py
+++ Code/checkDBCount.py
@@ -1,26 +1,19 @@
 import sqlite3
 
-# Connect to SQLite database (creates a new file if not exists)
-conn = sqlite3.connect('count.db')  # Replace 'your_database.db' with your actual database file
+conn = sqlite3.connect('count.db')
 
-# Create a cursor object using the connection
 cursor = conn.cursor()
 
-# SQL query to select all entries from mytable
 select_query = '''
     SELECT * FROM mytable;
 '''
 
-# Execute the query
 cursor.execute(select_query)
 
-# Fetch all rows from the result set
 rows = cursor.fetchall()
 
-# Print each row
 for row in rows:
     print(row)
 
-# Close the cursor and connection
 cursor.close()
 conn.close()
