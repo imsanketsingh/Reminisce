4636865e7a0c6e23b31147363b1a72782cb62e2f
imsanketsingh <imsanketsingh@gmail.com>
2024-07-08 13:42:33 +0530

    changes

--- Code/db.py
+++ Code/db.py
@@ -17,18 +17,20 @@ def database(articleName, event):
     # '''
 
     # cursor.execute(create_table_query)
+    articleName_quoted = f"'{articleName}'"
 
     update_query = ""
-    if(event):
-        update_query = f"UPDATE mytable SET liked = liked + 1 WHERE article = {articleName};"
+    if event:
+        update_query = "UPDATE mytable SET liked = liked + 1 WHERE article = ?;"
     else:
-        update_query = f"UPDATE mytable SET disliked = disliked + 1 WHERE article = {articleName};"
+        update_query = "UPDATE mytable SET disliked = disliked + 1 WHERE article = ?;"
 
-    cursor.execute(update_query)
+    cursor.execute(update_query, (articleName,))
 
-    # cursor.execute('SELECT * from mytable;')
-    # updated_rows = cursor.fetchall()
-    # for row in updated_rows:
-    #     st.markdown(f"{row[1]} has a {row[2]} likes and {row[3]} dislikes")
+    cursor.execute('SELECT * from mytable;')
+    updated_rows = cursor.fetchall()
+    for row in updated_rows:
+        st.markdown(f"{row[1]} has a {row[2]} likes and {row[3]} dislikes")
     
     conn.commit()
+    conn.close()
