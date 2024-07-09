6bdee5c3267b2f58d2afe40a0e5dfb9e79ae13ff
imsanketsingh <imsanketsingh@gmail.com>
2024-07-09 19:59:39 +0530

    changes

--- Code/functions.py
+++ Code/functions.py
@@ -423,7 +423,7 @@ def specialSubtopics():
             styles={
                 "container": {"padding": "10px", "background-color": "#f0f0f0", "border-radius": "8px"},
                 "icon": {"color": "#FF5733", "font-size": "14px", "margin-right": "10px"},
-                "nav-link": {"font-size": "16px", "text-align": "left", "margin": "10px", "padding": "8px", "border-radius": "6px", "transition": "background-color 0.3s ease", "color": "#333", "background-color": "#d4ab31"},
+                "nav-link": {"font-size": "16px", "text-align": "left", "margin": "10px", "padding": "8px", "border-radius": "6px", "transition": "background-color 0.3s ease", "color": "#333", "background-color": "#c1d431"},
                 "nav-link-selected": {"background-color": "#8bb52a", "color": "#fff"},
                 "nav-link:hover": {"background-color": "#FFC300", "color": "#fff"}
             },
